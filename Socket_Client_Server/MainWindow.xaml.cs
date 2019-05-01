c0da415be832e314ad19fd144e737e0d025efc5f
DouglasMarq <douglas.marq.alves@outlook.com>
2019-05-01 08:43:19 -0300

    Initial Commit

---
+++ Socket_Client_Server/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace Socket_Client_Server
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly BackgroundWorker worker = new BackgroundWorker();
+        private bool isClient;
+        private bool isServer;
+        private Server ServerWindow = new Server();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            worker.DoWork += worker_DoWork;
+            worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+            isClient = false;
+            isServer = false;
+        }
+
+        private void BtnClient_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isServer && !isClient)
+            {
+                //do something 
+                isClient = true;
+            }
+        }
+
+        private void BtnServer_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isClient && !isServer)
+            {
+                ServerWindow.Activate();
+                ServerWindow.Show();
+                isServer = true;
+                worker.RunWorkerAsync();
+            }
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void worker_RunWorkerCompleted(object sender,
+                                                   RunWorkerCompletedEventArgs e)
+        {
+            //update ui once worker complete his work
+        }
+
+    }
+}
---
+++ Socket_Client_Server/Server.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Socket_Client_Server
+{
+    /// <summary>
+    /// Interaction logic for Server.xaml
+    /// </summary>
+    public partial class Server : Window
+    {
+        public Server()
+        {
+            InitializeComponent();
+        }
+    }
+}
