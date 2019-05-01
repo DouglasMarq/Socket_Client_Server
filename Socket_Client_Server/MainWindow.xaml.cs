70d6b728147d7d91684ede67df0d1b05d018013b
DouglasMarq <douglas.marq.alves@outlook.com>
2019-05-01 10:11:21 -0300

    Server Side connection is accepting(hopefully)

--- Socket_Client_Server/MainWindow.xaml.cs
+++ Socket_Client_Server/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
 using System.Windows;
 
 namespace Socket_Client_Server
@@ -9,36 +11,88 @@ namespace Socket_Client_Server
     public partial class MainWindow : Window
     {
         private readonly BackgroundWorker worker = new BackgroundWorker();
-        private bool isClient;
-        private bool isServer;
-        private Server ServerWindow = new Server();
+        private string saveTxt;
+        private string saveTxt2;
 
         public MainWindow()
         {
             InitializeComponent();
             worker.DoWork += worker_DoWork;
             worker.RunWorkerCompleted += worker_RunWorkerCompleted;
-            isClient = false;
-            isServer = false;
+            txtBoxServerPort.GotFocus += TxtBoxServerPort_GotFocus;
+            txtBoxServerPort.LostFocus += TxtBoxServerPort_LostFocus;
+            txtBoxServerPort.TextChanged += TxtBoxServerPort_TextChanged;
+            txtBoxServerIP.GotFocus += TxtBoxServerIP_GotFocus;
+            txtBoxServerIP.LostFocus += TxtBoxServerIP_LostFocus;
+            txtBoxServerIP.TextChanged += TxtBoxServerIP_TextChanged;
+        }
+
+        private void TxtBoxServerPort_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtBoxServerPort.Text == "Insira a Porta")
+            {
+                saveTxt = txtBoxServerPort.Text;
+                txtBoxServerPort.Text = "";
+            }
+        }
+        private void TxtBoxServerPort_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (Regex.IsMatch(txtBoxServerPort.Text, "[^0-9]"))
+            {
+                txtBoxServerPort.Text = txtBoxServerPort.Text.Remove(txtBoxServerPort.Text.Length - 1);
+            }
+        }
+
+        private void TxtBoxServerPort_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtBoxServerPort.Text))
+            {
+                txtBoxServerPort.Text = saveTxt;
+            }
+        }
+
+        private void TxtBoxServerIP_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtBoxServerIP.Text == "Insira o IP")
+            {
+                saveTxt2 = txtBoxServerIP.Text;
+                txtBoxServerIP.Text = "";
+            }
+        }
+
+        private void TxtBoxServerIP_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtBoxServerIP.Text))
+            {
+                txtBoxServerIP.Text = saveTxt2;
+            }
+        }
+
+        private void TxtBoxServerIP_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (Regex.IsMatch(txtBoxServerIP.Text, "[^0-9-.]"))
+            {
+                txtBoxServerIP.Text = txtBoxServerIP.Text.Remove(txtBoxServerIP.Text.Length - 1);
+            }
         }
 
         private void BtnClient_Click(object sender, RoutedEventArgs e)
         {
-            if (!isServer && !isClient)
-            {
-                //do something 
-                isClient = true;
-            }
+
         }
 
         private void BtnServer_Click(object sender, RoutedEventArgs e)
         {
-            if (!isClient && !isServer)
+            if (!string.IsNullOrWhiteSpace(txtBoxServerIP.Text))
             {
+                Server ServerWindow = new Server(txtBoxServerIP.Text, Convert.ToInt32(txtBoxServerPort.Text));
                 ServerWindow.Activate();
-                ServerWindow.Show();
-                isServer = true;
-                worker.RunWorkerAsync();
+                //background check
+                //worker.RunWorkerAsync();
+            }
+            else
+            {
+                MessageBox.Show("Porta e/ou IP vazio", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
@@ -47,11 +101,9 @@ namespace Socket_Client_Server
 
         }
 
-        private void worker_RunWorkerCompleted(object sender,
-                                                   RunWorkerCompletedEventArgs e)
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-            //update ui once worker complete his work
-        }
 
+        }
     }
 }
--- Socket_Client_Server/Server.xaml.cs
+++ Socket_Client_Server/Server.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,9 +21,62 @@ namespace Socket_Client_Server
     /// </summary>
     public partial class Server : Window
     {
-        public Server()
+        public Server(string Ip, int Port)
         {
             InitializeComponent();
+            this.Show();
+            ip = Ip;
+            port = Port;
+            SetUpSocket();
+        }
+
+        private int port;
+        private string ip;
+        private TcpListener server;
+        private Socket connection;
+
+        private async void SetUpSocket()
+        {
+            try
+            {
+                txtBoxLog.AppendText("Iniciando Conexão em " + ip + " na porta " + port + ".");
+                server = new TcpListener(IPAddress.Parse(ip), port);
+                txtBoxLog.AppendText("\nConexão Aberta.");
+                server.Start();
+                await Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        connection = server.AcceptSocket();
+                    }
+                });
+            }
+            catch (ArgumentNullException)
+            {
+                
+                MessageBox.Show("Porta e/ou IP vazio.", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
+                this.Close();
+            }
+            catch (SocketException)
+            {
+                txtBoxLog.AppendText("\nErro.");
+                MessageBox.Show("Porta em uso.", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
+                this.Close();
+            }
+        }
+
+        private void BtnShutdown_Click(object sender, RoutedEventArgs e)
+        {
+            connection.Close();
+            server.Stop();
+            this.Close();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            connection.Close();
+            server.Stop();
+            base.OnClosed(e);
         }
     }
 }
